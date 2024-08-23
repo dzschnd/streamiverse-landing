07e39d46c4ff107c34e611666a7b6220e1a6d8a0
SafarzadeJunayd <safarzadejunayd@gmail.com>
2024-08-23 23:37:43 +0300

    added default styles to link at Privacy Policy page

--- src/pages/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -101,7 +101,7 @@ function PrivacyPolicy() {
             <p className={'doc-text'} style={{marginBottom: '16px'}}>
                 We use Google Analytics on our website. You can control and/or delete cookies if you wish. If
                 you want to know more about Google Analytics and its “do not track” policy, please
-                visit <a href={'https://www.google.com/analytics/terms/default.html'} target='_blank' rel="noreferrer">https://www.google.com/analytics/terms/default.html</a>
+                visit <a className={'a-default'} href={'https://www.google.com/analytics/terms/default.html'} target='_blank' rel="noreferrer">https://www.google.com/analytics/terms/default.html</a>
             </p>
             <p className={'doc-text fw-bold'}>Consent And Its Withdrawal</p>
             <p className={'doc-text'} style={{marginBottom: '16px'}}>
