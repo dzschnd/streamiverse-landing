d63fa171ee354d3c155739ff9c8092b736cf0437
SafarzadeJunayd <safarzadejunayd@gmail.com>
2024-08-23 22:21:24 +0300

    added Privacy Policy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,8 +15,8 @@ function Footer() {
                             <li><a href={'#'}>Follow Us</a></li>
                         </ul>
                         <ul className={'nav-set-2'}>
-                            <li><a href={'/terms-of-service'}>Terms of Service</a></li>
-                            <li><a href={'/privacy-policy'}>Privacy Policy</a></li>
+                            <li><a href={'/terms-of-service'} target='_blank'>Terms of Service</a></li>
+                            <li><a href={'/privacy-policy'} target='_blank'>Privacy Policy</a></li>
                         </ul>
                     </div>
                     <div className={'social-media'}>
