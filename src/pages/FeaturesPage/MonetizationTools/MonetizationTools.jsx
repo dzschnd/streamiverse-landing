29687bfc14d125bbbf673b321aee829e51a04d7f
SafarzadeJunayd <safarzadejunayd@gmail.com>
2024-09-16 18:16:14 +0300

    moved Coming Soon in mon tools

--- src/pages/FeaturesPage/MonetizationTools/MonetizationTools.jsx
+++ src/pages/FeaturesPage/MonetizationTools/MonetizationTools.jsx
@@ -138,9 +138,6 @@ function MonetizationTools({openWidget}) {
                         <img src={images.moneyTree} alt={''} id={'money-tree'}/>
                         <div>
                             <p>
-                                <div className={'color-primary-700'}>
-                                    Coming Soon
-                                </div>
                                 Explore various methods of monetization, such as selling digital assets, offering
                                 subscription services, and more
                             </p>
@@ -150,6 +147,9 @@ function MonetizationTools({openWidget}) {
                         <img src={images.streamerCoins} alt={''} id={'streamer-coins'}/>
                         <div>
                             <p>
+                                <div className={'color-primary-700'}>
+                                    Coming Soon
+                                </div>
                                 Create and distribute your own tokens to engage your community. Use them for special
                                 perks,
                                 access to exclusive content, or as a currency within your platform
