5adc3916d1e7d063a1bd597627fd76716f835038
SafarzadeJunayd <safarzadejunayd@gmail.com>
2024-09-18 13:21:52 +0300

    accounted for browser bookmark tab in the browser

--- src/pages/LoadingScreen/LoadingScreen.jsx
+++ src/pages/LoadingScreen/LoadingScreen.jsx
@@ -1,10 +1,29 @@
-import './LoadingScreen.css'
+import { useEffect, useState } from 'react';
+import './LoadingScreen.css';
+
 function LoadingScreen() {
+    const [vh, setVh] = useState(window.innerHeight * 0.01);
+
+    useEffect(() => {
+        const updateVh = () => {
+            const newVh = window.innerHeight * 0.01;
+            setVh(newVh);
+        };
+
+        updateVh();
+
+        window.addEventListener('resize', updateVh);
+
+        return () => {
+            window.removeEventListener('resize', updateVh);
+        };
+    }, []);
+
     return (
-        <div className={'loading-screen background-neutral-900'}>
+        <div className="loading-screen background-neutral-900" style={{ height: `calc(${vh}px * 100)` }}>
 
         </div>
-    )
+    );
 }
 
 export default LoadingScreen;
