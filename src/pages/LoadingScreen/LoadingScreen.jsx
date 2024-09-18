5d2f9dd608ab153568a24736f2d0bb38ae0a9af8
SafarzadeJunayd <safarzadejunayd@gmail.com>
2024-09-18 14:02:41 +0300

    another implementation of disabling safari bounce effect

--- src/pages/LoadingScreen/LoadingScreen.jsx
+++ src/pages/LoadingScreen/LoadingScreen.jsx
@@ -1,50 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './LoadingScreen.css';
+
+function ScrollFix(elem) {
+    var startY, startTopScroll;
+
+    // If there is no element, then do nothing
+    if (!elem) return;
+
+    // Handle the start of interactions
+    elem.addEventListener('touchstart', function (event) {
+        startY = event.touches[0].pageY;
+        startTopScroll = elem.scrollTop;
+
+        if (startTopScroll <= 0)
+            elem.scrollTop = 1;
+
+        if (startTopScroll + elem.offsetHeight >= elem.scrollHeight)
+            elem.scrollTop = elem.scrollHeight - elem.offsetHeight - 1;
+    }, false);
+}
 
 function LoadingScreen() {
     const [vh, setVh] = useState(window.innerHeight * 0.01);
-
-    // Function to prevent scrolling (used on mobile for touchmove)
-    const preventDefault = (e) => {
-        e.preventDefault();
-    };
-
-    // Disable scrolling by adding touchmove listener
-    const disableScroll = () => {
-        document.body.addEventListener('touchmove', preventDefault, { passive: false });
-    };
-
-    // Re-enable scrolling by removing the listener
-    const enableScroll = () => {
-        document.body.removeEventListener('touchmove', preventDefault);
-    };
+    const loadingScreenRef = useRef(null);
 
     useEffect(() => {
-        // Function to update the viewport height
         const updateVh = () => {
             const newVh = window.innerHeight * 0.01;
             setVh(newVh);
         };
 
-        // Update the height initially
         updateVh();
 
-        // Disable scrolling
-        disableScroll();
-
-        // Add event listener for resizing window
+        if (loadingScreenRef.current) {
+            ScrollFix(loadingScreenRef.current);
+        }
         window.addEventListener('resize', updateVh);
-
-        // Clean up: remove event listener and enable scrolling again on unmount
         return () => {
             window.removeEventListener('resize', updateVh);
-            enableScroll(); // re-enable scrolling when the component unmounts
         };
-    }, []); // Empty dependency array to ensure this runs only on mount/unmount
+    }, []);
 
     return (
-        <div className="loading-screen background-neutral-900" style={{ height: `calc(${vh}px * 100)` }}>
-            {/* Loading screen content */}
+        <div ref={loadingScreenRef} className="loading-screen background-neutral-900" style={{ height: `calc(${vh}px * 100)` }}>
+
         </div>
     );
 }
